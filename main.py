4ba04cb860c889cdb38a0c5bb09aa5b09e562f66
mike <yangxue088@gmail.com>
2016-04-22 10:04:55 +0800

    修正bug

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 from scrapy import optional_features
 from scrapy.crawler import CrawlerProcess
 
-from spiders.category import CategorySpider
+from spiders.feedback import FeedbackSpider
+from spiders.order import OrderSpider
 
 optional_features.remove('boto')
 
@@ -11,23 +12,26 @@ settings = {'COOKIES_ENABLED': False, 'ITEM_PIPELINES': {
     'pipelines.DuplicatePipeline': 200,
     'pipelines.ToRedisPipeline': 300,
     'pipelines.ToMongoPipeline': 400,
-}, 'LOG_LEVEL': 'INFO', 'prefix': 'searchusbinusbflashdrivers',
-            'base_url': 'http://www.aliexpress.com/wholesale?catId=711005&SearchText=usb',
-            # 'DOWNLOADER_MIDDLEWARES': {'scrapy_crawlera.CrawleraMiddleware': 600},
-            # 'CRAWLERA_ENABLED': True,
-            # 'CRAWLERA_USER': '29935fa9eb5c404985b6ad6cacc6a30e',
-            # 'CRAWLERA_PASS': '',
-            # 'CONCURRENT_REQUESTS': 10,
-            # 'CONCURRENT_REQUESTS_PER_DOMAIN': 10,
-            # 'AUTOTHROTTLE_ENABLED': False,
-            # 'DOWNLOAD_TIMEOUT': 600
-            }
+}, 'LOG_LEVEL': 'INFO', 'prefix': 'earphonesheadphones',
+            'base_url': 'http://www.aliexpress.com/category/200003130/armbands.html'}
+
+crawlera_settings = dict(settings)
+crawlera_settings.update({'DOWNLOADER_MIDDLEWARES': {'scrapy_crawlera.CrawleraMiddleware': 600},
+                          'CRAWLERA_ENABLED': True,
+                          'CRAWLERA_USER': '29935fa9eb5c404985b6ad6cacc6a30e',
+                          'CRAWLERA_PASS': '',
+                          'CONCURRENT_REQUESTS': 10,
+                          'CONCURRENT_REQUESTS_PER_DOMAIN': 10,
+                          'AUTOTHROTTLE_ENABLED': False,
+                          'DOWNLOAD_TIMEOUT': 600})
 
 process = CrawlerProcess(settings)
-process.crawl(CategorySpider)
+# process = CrawlerProcess(crawlera_settings)
+
+# process.crawl(CategorySpider, lambda rate, order: rate >= 100 or order >= 100)
 # process.crawl(ProductSpider)
-# process.crawl(FeedbackSpider)
-# process.crawl(OrderSpider)
+process.crawl(FeedbackSpider)
+process.crawl(OrderSpider)
 # process.crawl(StoreSpider)
 
 process.start()
