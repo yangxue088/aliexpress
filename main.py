cf32d177554ae629d5069bb5cd99b551e7302e88
mike <yangxue088@gmail.com>
2016-04-27 16:02:39 +0800

    补全代码

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
+import sys
 
 from scrapy import optional_features
 from scrapy.crawler import CrawlerProcess
 
-from spiders.feedback import FeedbackSpider
-from spiders.order import OrderSpider
-from spiders.store import StoreSpider
+from spiders.category import CategorySpider
 
 optional_features.remove('boto')
 
@@ -19,7 +18,7 @@ settings = {'TELNETCONSOLE_ENABLED': False, 'COOKIES_ENABLED': False, 'ITEM_PIPE
 crawlera_settings = dict(settings)
 crawlera_settings.update({'DOWNLOADER_MIDDLEWARES': {'scrapy_crawlera.CrawleraMiddleware': 600},
                           'CRAWLERA_ENABLED': True,
-                          'CRAWLERA_USER': '29935fa9eb5c404985b6ad6cacc6a30e',
+                          'CRAWLERA_USER': sys.argv[1],
                           'CRAWLERA_PASS': '',
                           'CONCURRENT_REQUESTS': 10,
                           'CONCURRENT_REQUESTS_PER_DOMAIN': 10,
@@ -27,13 +26,15 @@ crawlera_settings.update({'DOWNLOADER_MIDDLEWARES': {'scrapy_crawlera.CrawleraMi
                           'DOWNLOAD_TIMEOUT': 600})
 
 process = CrawlerProcess(settings)
-# process = CrawlerProcess(crawlera_settings)
+process.crawl(CategorySpider, lambda rate, order: rate >= 100 or order >= 100)
 
-# process.crawl(CategorySpider, lambda rate, order: rate >= 100 or order >= 100)
+# process = CrawlerProcess(crawlera_settings)
 # process.crawl(ProductSpider)
-for i in xrange(100):
-    process.crawl(FeedbackSpider)
-    process.crawl(OrderSpider)
-process.crawl(StoreSpider)
+
+# for i in xrange(100):
+#     process.crawl(FeedbackSpider)
+#     process.crawl(OrderSpider)
+#
+# process.crawl(StoreSpider)
 
 process.start()
